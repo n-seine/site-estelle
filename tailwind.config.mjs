2536c64dbbec44ebe7564fa1ef45114bfd2e45c3
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-01-29 22:28:11 +0100

    basic homepage

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        serif: ["PT serif", ...defaultTheme.fontFamily.serif],
+      },
+      colors: { primary: "#e06d5e" },
+    },
+  },
+  plugins: [],
+};
