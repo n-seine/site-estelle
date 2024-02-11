82b9c7febddbaf15a36a0b44a5c0a249f773511b
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-02-11 19:13:23 +0100

    import env diferently

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import storyblok from "@storyblok/astro";
 import netlify from "@astrojs/netlify";
 
-import { loadEnv } from "vite";
-const env = loadEnv("", process.cwd(), "STORYBLOK");
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://astro.build/config
 export default defineConfig({
-  output: env.STORYBLOK_IS_PREVIEW === "yes" ? "server" : "static",
+  output: import.meta.env.STORYBLOK_IS_PREVIEW === "yes" ? "server" : "static",
 
   vite: {
     plugins: [basicSsl()],
@@ -23,15 +21,15 @@ export default defineConfig({
     tailwind(),
     storyblok({
       accessToken:
-        env.STORYBLOK_IS_PREVIEW === "yes"
-          ? env.STORYBLOK_PREVIEW_TOKEN
-          : env.STORYBLOK_PUBLIC_TOKEN,
+        import.meta.env.STORYBLOK_IS_PREVIEW === "yes"
+          ? import.meta.env.STORYBLOK_PREVIEW_TOKEN
+          : import.meta.env.STORYBLOK_PUBLIC_TOKEN,
       components: {
         projectDetails: "components/storyblok/ProjectDetails",
         portfolioGrid: "components/storyblok/PortfolioGrid",
         page: "components/storyblok/Page",
       },
-      bridge: env.STORYBLOK_IS_PREVIEW === "yes" ? true : false,
+      bridge: import.meta.env.STORYBLOK_IS_PREVIEW === "yes" ? true : false,
     }),
     react(),
   ],
